997f0c88d00c5c7cf68d09b7f1d318f4a8ecf4f1
peterladur <peter.ladour@gmail.com>
2026-01-18 20:34:25 +1300

    commented parts of the generate_states_and_q_table_perfect_opponent.py

--- Part 1: Tic Tac Toe/generate_states_and_q_table_perfect_opponent.py
+++ Part 1: Tic Tac Toe/generate_states_and_q_table_perfect_opponent.py
@@ -3,8 +3,8 @@ import time
 import pprint
 import json
 
-def find_maximum(entries):
-
+def find_maximum(entries:list):
+    """finds the maximum of a list ignoring null values"""
     if entries == None:
         return -1
 
@@ -15,8 +15,8 @@ def find_maximum(entries):
 
     return maximum
 
-def find_minimum(entries):
-
+def find_minimum(entries:list):
+    """finds the minimum of a list ignoring null values"""
     if entries == None:
         return 1
     
@@ -28,18 +28,26 @@ def find_minimum(entries):
     return minimum
 
 
-def check_if_valid(state:str):
-    """checks if a given state is possible"""
+def check_state(state:str):
+    """Checks the given state
 
-    """check if the number x, o is correct"""
+    Output:
+    None: Invalid State
+    -1: Win for O
+    0: Drawn
+    1: Win for X
+    2: Undetermined
+    """
+    
     x_number = state.count('x')
     o_number = state.count('o')
     
+    #check if the number of x, o is correct
     if (x_number < o_number) or (x_number - o_number) > 1:
         return None
     
     
-    """check if the there if there is more then one win on the board"""
+    #check if at most 1 player has won
     x_win = False
     o_win = False
     
@@ -53,20 +61,20 @@ def check_if_valid(state:str):
 
     
     for i in range(3):
-        row = state[0 + 3*i] + state[1 + 3*i] + state[2 + 3*i]
-        col = state[0 + i] + state[3 + i] + state[6 + i]
+        row = state[0 + 3*i] + state[1 + 3*i] + state[2 + 3*i] #checks each row
+        col = state[0 + i] + state[3 + i] + state[6 + i] #checks each column
         if (row == 'xxx') or (col == 'xxx'):
             x_win = True
         if (row == 'ooo') or (col == 'ooo'):
             o_win = True
     
-    if (o_win and (x_number > o_number)):
+    if (o_win and (x_number > o_number)): #O can only win provided x_number == o_number
         return None
 
-    if(x_win and (x_number == o_number)):
+    if(x_win and (x_number == o_number)): #X can only win provided x_number == o_number + 1
         return None
 
-    if (x_win and o_win):
+    if (x_win and o_win): #both players cannot win
         return None
 
     if (x_win):
@@ -83,7 +91,7 @@ def check_if_valid(state:str):
 
 def display_state(state: str, message=""):
     """displays state as a 3x3"""
-    print(message)
+    print(message) #Useful for debugging
     print(state[:3])
     print(state[3:6])
     print(state[6:])
@@ -102,28 +110,27 @@ def generate_all_possible_states():
     unique_combinations = set(permutations(digits))
     unique_combinations =  {''.join(x) for x in unique_combinations}
 
-    for combination in unique_combinations:
-        if check_if_valid(combination) != None:
-            Q_Table[combination] = [check_if_valid(combination)] * 9
+    for combination in unique_combinations: #checks each possible permutation for validity
+        if check_state(combination) != None:
+            Q_Table[combination] = [check_state(combination)] * 9 #if the state is valid, it adds it to the Q-Table (check_state == result of a completely finished game)
 
-    possible_states = dict()
+    possible_states = dict() #possible states, stores moves 0-9 mapped to a list of all possible states for that number of moves
     possible_states[9] = unique_combinations
     
-    #for combination in possible_states[9]:
-        #Q_Table.setdefault(combination, [check_if_valid(combination)] * 9)
 
-
-    """part 2 will remove an x or a o, step by step"""
+    """part 2 will remove an x or a o, step by step"
+    
+    "parent combination" is the original combination from which a symbol is removed """
     for turn in range(8, -1, -1):
-        #print(turn)
         possible_states[turn] = set()
 
-        #depends on which turn it is
+        #the symbol removed depends on which turn it is
         symbol = "x"
         if (turn % 2 == 1):
             symbol = "o"
 
-        for combination in possible_states[turn + 1]: #goes through each parent combination
+
+        for combination in possible_states[turn + 1]: #goes through each "parente combination"
             list_combination = list(combination) 
             for i, char in enumerate(list_combination): #for each x/o in the combination, it removes it, and adds the possibility to the list
                 if char == symbol:
@@ -134,14 +141,14 @@ def generate_all_possible_states():
                     possible_states[turn].add(str_combination)
 
                     #if the state is physically possible, generates an entry in the Q Table for it
-                    result = check_if_valid(str_combination)
+                    result = check_state(str_combination)
                     if result != None: #if state is valid 
                         Q_Table.setdefault(str_combination, [None] * 9)
                         if result != 2: #if the game state is a determined state
                             Q_Table[str_combination] = [result] * 9 #give it the determined value
 
                         else:
-                            validity = check_if_valid(combination) #we want to see what is the result of the original combination
+                            validity = check_state(combination) #we want to see what is the result of the original combination
                             
                             if validity in {-1, 1, 0}:
                                 Q_Table[str_combination][i] = validity
@@ -161,12 +168,12 @@ def generate_all_possible_states():
     undetermined = 0
 
     for turn in possible_states:
-        possible_states[turn] = {x for x in possible_states[turn] if (check_if_valid(x) != None)}
+        possible_states[turn] = {x for x in possible_states[turn] if (check_state(x) != None)}
 
     all_states = []
     for turn in possible_states:
         for position in possible_states[turn]:
-            result = check_if_valid(position)
+            result = check_state(position)
             if result == 1:
                 x_wins +=1
             if result == -1:
